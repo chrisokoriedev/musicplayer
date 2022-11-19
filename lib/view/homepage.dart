99050bb8b05ae1dcb79bf8332bf8c15793068d22
ChrisIuil <okoriec01@gmail.com>
2022-11-19 09:54:44 +0100

    music list

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -8,7 +11,46 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int currentIndex = 0;
+  final FlutterAudioQuery audioQuery = FlutterAudioQuery();
+  List<SongInfo> songs = [];
+  // final GlobalKey<MainMusicSectionState> key =
+  //     GlobalKey<MainMusicSectionState>();
+
+  final ScrollController scrollController = ScrollController();
   double height = 50;
+
+  @override
+  void initState() {
+    super.initState();
+    getTrack();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    songs.clear();
+  }
+
+  void getTrack() async {
+    songs = await audioQuery.getSongs();
+    setState(() {
+      songs = songs;
+    });
+  }
+
+  void changeTrack(bool isNext) {
+    if (isNext) {
+      if (currentIndex != songs.length - 1) {
+        currentIndex++;
+      }
+    } else {
+      if (currentIndex != 0) {
+        currentIndex--;
+      }
+    }
+    // key.currentState.setSong(songs[currentIndex]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +58,45 @@ class _HomepageState extends State<Homepage> {
       appBar: AppBar(
         toolbarHeight: 1.0,
       ),
-      body: SingleChildScrollView(
-          child: Column(children: [Center(child: Text('hey'))])),
+      body: ListView.separated(
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Color(0xff000e24),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xff043c4a).withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    )
+                  ]),
+              child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/placeholder.png'),
+                  ),
+                  title: Text(
+                    songs[index].title,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Icon(
+                    Icons.more_horiz,
+                    color: Colors.white,
+                  ),
+                  subtitle: Text(songs[index].artist,
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold)),
+                  onTap: () {
+                    currentIndex = index;
+                  }),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: songs.length),
       bottomSheet: GestureDetector(
         onTap: () {
           setState(() {
